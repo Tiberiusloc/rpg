35d1f7194da4a00a3b1c6cd64f497584441533cb
Tiberius Lockett <tlockett680@gmail.com>
2022-08-31 10:33:05 -0700

    pass 1st test to create new character obj w our values
    Co-authored-by: Kelly Bruce <kelly.bruce.cs@gmail.com>

---
+++ src/rpg.js
@@ -0,0 +1,14 @@
+export class Character {
+  constructor(name, level, exp, inventory, alignment, job, hp, attack, defense, money) {
+    this.name = name
+    this.level = level
+    this.exp = exp
+    this.inventory = inventory
+    this.alignment = alignment
+    this.job = job
+    this.hp = hp
+    this.attack = attack
+    this.defense = defense
+    this.money = money
+  }
+}
