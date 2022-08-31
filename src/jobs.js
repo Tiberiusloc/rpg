af8165bcd72a0d8eb724e2dc92839c2456084262
Tiberius Lockett <tlockett680@gmail.com>
2022-08-31 15:04:06 -0700

    pass test 3 for npc
    Co-authored-by: Kelly Bruce <kelly.bruce.cs@gmail.com>

---
+++ src/jobs.js
@@ -0,0 +1,3 @@
+import {Job} from './rpg.js'
+
+export let wizard = new Job("Wizard", ["Starter Staff", "Starter Pointy Hat", "Starter Robes",])
--- src/rpg.js
+++ src/rpg.js
@@ -1,5 +1,5 @@
 export class Character {
-  constructor(name, level, exp, inventory, alignment, job, hp, attack, defense, money) {
+  constructor(name, level, exp, inventory, alignment, job, hp, attack, defense, money,) {
     this.name = name
     this.level = level
     this.exp = exp
@@ -10,11 +10,25 @@ export class Character {
     this.attack = attack
     this.defense = defense
     this.money = money
+    this.quest = [];
+  }
+  addQuest(array) {
+    array.forEach((quest) => {
+      this.quest.push(quest);
+    })
   }
 }
 
 export class Job {
-  constructor (inventory, job) {
-  
+  constructor (job, inventory) {
+  this.job = job
+  this.inventory = inventory
+  }
+}
+
+export class Npc {
+  constructor (name, quest) {
+  this.name = name
+  this.quest = quest
   }
 }
